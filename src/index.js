7baee6f80482c3130a7f9a390dbbbc6e7d9d76db
Joel Bernasky <joel_bernasky@homedepot.com>
2018-12-07 12:49:03 -0500

    repository initialization

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// client/src/App.js
+import React, { Component } from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  render() {
+    return <div>I'M READY TO USE THE BACK END APIS! :-)</div>;
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default App;
